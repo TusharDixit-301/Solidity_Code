d0cec33c49eacc093b06bc9275aab7aced724063
Tushar Dixit <37350387+TusharDixit-301@users.noreply.github.com>
2022-04-12 01:17:44 +0530

    Added

---
+++ Constant.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract chckconstant{
+    
+    address constant public myadd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public myadd2 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    //difference is execution cost
+}
